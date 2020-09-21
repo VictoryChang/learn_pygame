b424fe28085e4097c68105f1d52ab9d2232667ca
Victory Chang <victorychang@outlook.com>
2020-09-20 20:04:50 -0700

    Use template menu from https://www.youtube.com/watch?time_continue=239&v=0RryiSjpJn0&feature=emb_logo

--- main_menu.py
+++ main_menu.py
@@ -1,29 +1,95 @@
-import pygame
+import sys
 
-from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUIT
+from pygame.locals import *
+import pygame
 
 from game.constants import Color
 from game.utility import draw_text
 
+main_clock = pygame.time.Clock()
 
-def main_menu(screen_width: int = 800, screen_height: int = 600):
-    pygame.init()
 
-    screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.init()
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((500, 500), 0, 32)
 
-    running = True
+click = False
 
-    while running:
+
+def main_menu():
+    while True:
+        screen.fill(Color.black)
+        draw_text('main menu', Color.white, screen, 20, 20)
+
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        button_2 = pygame.Rect(50, 200, 200, 50)
+
+        if button_1.collidepoint((mouse_x, mouse_y)):
+            if click:
+                game()
+
+        if button_2.collidepoint((mouse_x, mouse_y)):
+            if click:
+                options()
+
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+        pygame.draw.rect(screen, (255, 0, 0), button_2)
+
+        click = False
         for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        pygame.display.update()
+        main_clock.tick(60)
+
+
+def game():
+    running = True
+    while running:
+        screen.fill(Color.black)
+
+        draw_text('game', Color.white, screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
-            elif event.type == QUIT:
-                running = False
 
-            screen.fill(Color.black)
-            draw_text(text='main menu', color=Color.white, surface=screen, x=20, y=20)
-            pygame.display.flip()
+        pygame.display.update()
+        main_clock.tick(60)
+
+
+def options():
+    running = True
+    while running:
+        screen.fill(Color.black)
+
+        draw_text('options', Color.white, screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+        main_clock.tick(60)
 
 
 main_menu()
